dbf5c00ee1a013aefa392bf75665e0e5885c2723
Mike Heft <mikeheft@gmail.com>
2017-10-18 10:26:51 -0600

    Get api key to work

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -6,7 +6,11 @@ class SearchService
 
 
   def self.find_alt
-    @conn = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?location=80203&api_key=ENV['nrel_api']")
+    @conn = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest") do |faraday|
+      faraday.params['api_key'] = ENV['nrel_api']
+      faraday.params['location'] = '80203'
+    end
+
 require 'pry'; binding.pry
   end
 
